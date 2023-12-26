746f1a0e0b1dd02d2f6fb65c8d9f8b2c8c9f1d43
Gkibria <gkibria121@gmail.com>
2023-12-26 11:24:34 +0600

    state created for capter selection

--- src/ResumeBuilder/ResumeComponents/ResumeFinalize/ResumeOthers.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeFinalize/ResumeOthers.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+
+
+
 
 function ResumeOthers() {
+    const [inputType,setInputType] = useState('projects') 
+
+    const handleChange = (e)=>{
+        setInputType(e.target.value)
+        console.log(inputType)
+    }
+
+
+
+
   return (
     <div>
     <ul className="addmore">
         <li>
-            <input type="radio" id="projects" name="add" />
+            <input type="radio" value="projects" name="add" checked={inputType=='projects'}   onChange={handleChange}/>
             <label htmlFor="projects">Add projects</label>
         </li>
         <li>
-            <input type="radio" id="certificates" name="add" />
+            <input type="radio" value="certificates" name="add" onChange={handleChange}/>
             <label htmlFor="certificates">Add certificates</label>
         </li>
         <li>
-            <input type="radio" id="publications" name="add" />
+            <input type="radio" value="publications" name="add" onChange={handleChange} />
             <label htmlFor="publications">Add publications</label>
         </li>
         <li>
-            <input type="radio" id="hobbies" name="add" />
+            <input type="radio" value="hobbies" name="add" onChange={handleChange}/>
             <label htmlFor="hobbies">Add hobbies</label>
         </li>
         <li>
-            <input type="radio" id="more" name="add" />
+            <input type="radio" value="more" name="add" onChange={handleChange}/>
             <label htmlFor="more">Other</label>
-            <input type="text" id="more" />
+            {/* <input type="text" value="more" /> */}
         </li>
     </ul>
     <button className='addmore-btn'>Add New</button>
