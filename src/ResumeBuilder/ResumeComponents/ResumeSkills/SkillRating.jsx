4356358e48f6b7d1b8c797d29425e8d82ca426ba
Gkibria <gkibria121@gmail.com>
2023-12-26 11:26:36 +0600

    style added

--- src/ResumeBuilder/ResumeComponents/ResumeSkills/SkillRating.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeSkills/SkillRating.jsx
@@ -1,38 +1,44 @@
-'use client'
-import React, { useCallback, useEffect, useState } from 'react'
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import { HiMinus, HiPlus } from "react-icons/hi";
 
-function SkillRating({setCurrentSkill,currentSkill,rate_title}) {
-
-
-    const increase_rating = useCallback(()=>{
-
-      if(currentSkill.rating<5){
-        setCurrentSkill(prev => {return {...prev, rating : prev.rating+0.5}})
-        console.log(currentSkill)
-
-      }
-    },[currentSkill]);
-    const decrease_rating = useCallback(()=>{
-      if(currentSkill.rating>0){
-        setCurrentSkill(prev => {return {...prev, rating : prev.rating-0.5}})
-        
-      }
-    },[currentSkill]);
+function SkillRating({ setCurrentSkill, currentSkill, rate_title }) {
+  const increase_rating = useCallback(() => {
+    if (currentSkill.rating < 5) {
+      setCurrentSkill((prev) => {
+        return { ...prev, rating: prev.rating + 0.5 };
+      });
+      console.log(currentSkill);
+    }
+  }, [currentSkill]);
+  const decrease_rating = useCallback(() => {
+    if (currentSkill.rating > 0) {
+      setCurrentSkill((prev) => {
+        return { ...prev, rating: prev.rating - 0.5 };
+      });
+    }
+  }, [currentSkill]);
   return (
-    <div>          
-    <label htmlFor="rating">{rate_title}</label>
-    <div>
-      <button onClick={increase_rating}>+</button>
-      <button onClick={decrease_rating}>-</button>
+    <div
+      style={{ display: "grid", gridTemplateColumns: "200px 50px 50px 50px" }}
+    >
+      <div>
+        <label htmlFor="rating">{rate_title}</label>
+      </div>
+      <div style={{ marginInline: "auto",cursor : 'pointer' }} onClick={decrease_rating}>
+        <HiMinus />
+      </div>
+      <input
+        style={{ textAlign: "center", height: "10px", marginTop: "10px" }}
+        type="text"
+        value={currentSkill.rating}
+        readOnly
+      />
+      <div style={{ marginInline: "auto",cursor : 'pointer' }} onClick={increase_rating}>
+        <HiPlus />
+      </div>
     </div>
-    <input
-      type="text"
-      value={currentSkill.rating}
-      readOnly
-    />
-    
-    </div>
-  )
+  );
 }
 
-export default SkillRating
+export default SkillRating;
