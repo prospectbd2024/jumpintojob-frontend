0ce7a80a7b25917614d532eafa9ad116be3f3f26
Ahmed Kibria <gkibria121@gmail.com>
2024-02-14 11:27:35 +0600

    test removed

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,6 @@ function layout({children}) {
   return (
     <html lang="en">
       <body className='fou'>
-      test is auto deploy is working or not
       <UserProvider>
       <JobContext>
         {children}
