f2c0b8c750076da93b9c53aab921daec47f49f51
Ahmed Kibria <gkibria121@gmail.com>
2024-02-25 13:27:09 +0600

    handle google login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,25 +1,15 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const handler =  NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      // Customize user object with additional data (optional)
-      return user;
-    },
-    async session({ session, user }) {
-      // Add user data to session if needed
-      session.user = user;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET // Replace with a secure secret
-});
-
-export {handler as GET, handler as POST}
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+export const authOptions = {
+ providers: [
+  GoogleProvider({
+   clientId: process.env.GOOGLE_CLIENT_ID,
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  }),
+ ],
+ session: {
+  strategy: 'jwt',
+ },
+};
+const handler =  NextAuth(authOptions)
+export {handler as GET,handler as POST};
