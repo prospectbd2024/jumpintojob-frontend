97215576ea297cb6571b703fb33149461bb53bb0
Ahmed Kibria <gkibria121@gmail.com>
2024-03-31 14:44:14 +0600

    separated components

--- src/Components/Companies/Companies.jsx
+++ src/Components/Companies/Companies.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import Link from "next/link";
+import CategoryList from "./CategoryList";
+import CompanyListView from "./CompanyListView";
 
 const Companies = () => {
   const [companies, setCompanies] = useState([]);
@@ -47,89 +47,10 @@ const Companies = () => {
       </div>
       <div className="companies-content container">
         <div className="companies-tabs">
-          <div className="companies-tablist">
-            <div
-              key={"all"}
-              onClick={() => handleCategoryChange("all")}
-              className={`${
-                selectedCategory == "all"
-                  ? "company-category-selected"
-                  : "company-tab"
-              }`}
-            >
-              All Industries
-            </div>
-            {categories.map((category) => {
-              return (
-                <div
-                  key={category.category_name}
-                  onClick={() => handleCategoryChange(category.id)}
-                  className={`${
-                    selectedCategory == category.id
-                      ? "company-category-selected"
-                      : "company-tab"
-                  }`}
-                >
-                  {category.category_name}
-                </div>
-              );
-            })}
-            <div
-              key={"others"}
-              onClick={() => handleCategoryChange("others")}
-              className={`${
-                selectedCategory == "others"
-                  ? "company-category-selected"
-                  : "company-tab"
-              }`}
-            >
-              Other
-            </div>
-          </div>
+        <CategoryList props={{handleCategoryChange, selectedCategory, categories}} />
+        <CompanyListView props={{companies}} />
 
 
-              <div className="companies-tabs-content">
-                {companies.map((company)=>{
-                  return <div key={company.name} className="company-item">
-                     <div className="company-item-content">
-                    <div className="company-item-content-banner">
-                      <img src={company.cover_image} alt="" />
-                    </div>
-                    <div className="company-item-content-main">
-                      <div className="main-items">
-                        <img src={company.logo} alt="" />
-                        <div>
-                          <Link href={""}>
-                            <h3>{company.name}</h3>
-                          </Link>
-                          <p>Verified Profile</p>
-                        </div>
-                      </div>
-                      <div className="main-items">
-                        <p>{company.company_category}</p>
-                        { company.size?<p>Company Size: {company.size}</p>: ''}
-                      </div>
-                      <div className="main-items">
-                        <p className="company_description">
-                          {company.description? company.description.slice(0, 140)+'...':''}
-                        </p>
-                      </div>
-                      <div className="company-item-content-footer">
-                        <Link href={company.links.show}>
-                          <button className="company-button company-details-button">
-                            View Details
-                          </button>
-                        </Link>
-                        <Link href={""}>
-                          <button className="company-button company-jobs-button">
-                            View Jobs
-                          </button>
-                        </Link>
-                      </div>
-                    </div>
-                  </div></div>
-                })}
-              </div>
         </div>
       </div>
     </div>
