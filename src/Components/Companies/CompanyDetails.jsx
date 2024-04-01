e40ca0f7418ce4f5f787d6a4b931f245c843aef7
Ahmed Kibria <gkibria121@gmail.com>
2024-04-01 19:31:44 +0600

    company details component created

---
+++ src/Components/Companies/CompanyDetails.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function CompanyDetails({company}) {
+  return (
+    <div key={company.name} className="company-item">
+    <div className="company-item-content">
+        <div className="company-item-cover company-cover">
+            <img src={company.cover_image} alt="" className="company-cover-image" />
+        </div>
+        <div className="company-item-details">
+            <div className="company-logo company-logo-container">
+                <img src={company.logo} alt="" className="company-logo-image" />
+                <div className="company-info company-info-container">
+                        <h3 className="company-title">{company.name}</h3>
+                    <p className="company-status">Verified Profile</p>
+                </div>
+            </div>
+            <div className="company-category">
+                <p className="company-category-text">{company.category? company.category :''}</p>
+                {company.size && <p className="company-size">Company Size: {company.size}</p>}
+            </div>
+        </div>
+        <div className="company-description company-description-container">
+            <p className="company-description-text">{company.description ? company.description : ''}</p>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default CompanyDetails
