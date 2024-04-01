4126012e77d6ab27c1cbf4eb0dc2c66c63c691e4
Ahmed Kibria <gkibria121@gmail.com>
2024-04-01 19:29:15 +0600

    redirect to job details if viewd any job

--- app/(seeker)/findjobs/page.jsx
+++ app/(seeker)/findjobs/page.jsx
@@ -1,9 +1,24 @@
+"use client"
 import DefaultJobDetails from '@/Components/AllJobs/DefaultJobDetails'
-import React from 'react'
+import {  useJobContext } from '@/jobContext/JobContext'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 function Page() {
+  const router = useRouter()
+
+   const {selectedJob} = useJobContext();
+   useState(()=>{
+     if (selectedJob.id) {
+      router.push('/findjobs/jobdetails/'+selectedJob.id)
+     }
+
+   },[])
   return (
-    <DefaultJobDetails/>
+    <div>
+      {!selectedJob.id &&
+      <DefaultJobDetails/>}
+    </div>
   )
 }
 
