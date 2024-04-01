ee246b2a403efa1737b3f1aef682c8b18e195779
Ahmed Kibria <gkibria121@gmail.com>
2024-04-01 23:37:13 +0600

    cache categories with companies

--- src/Contexts/CompanyContext.jsx
+++ src/Contexts/CompanyContext.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useContext,createContext, useState,useEffect, useCallback} from 'react'
+import React,{useContext,createContext, useState,useEffect, useCallback, cache} from 'react'
 
 
 export const companyContext = createContext();
@@ -8,20 +8,48 @@ export const useCompanyContext = ()=> useContext(companyContext);
 
 
 function CompanyContext({children}) {
+
     const [companies, setCompanies] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [cachedCategoryCompanies,addCachedCategoryCompanes] = useState([]);
+
+    const handleCachedCompanies = useCallback((category_id)=>{
+      console.log(category_id);
+      let flag = false;
+      let companies = [];
+      cachedCategoryCompanies.map((categoryCompanies)=>{
+        if(categoryCompanies.category_id==category_id){
+          flag = true;
+          companies = categoryCompanies.companies;
+        }
+      })
+      if(!flag){
+        fetchCompanes(category_id)
+      }
+      else{
+        setCompanies(companies)
+      }
+    },[cachedCategoryCompanies])
     const getCompanies = useCallback((id) => {
-        const queryParams = {
-          category_id: id
-        };
-        const queryString = new URLSearchParams(queryParams).toString();
-        console.log(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/companies?${queryString}`);
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/companies?${queryString}`)
-          .then((res) => res.json())
-          .then((data) => {
-            setCompanies(data.data);
-          });
+      handleCachedCompanies(id)
       });
+    
+    const fetchCompanes = useCallback((category_id)=>{
+      const queryParams = {
+        category_id: category_id
+      };
+      const queryString = new URLSearchParams(queryParams).toString();
+      console.log(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/companies?${queryString}`);
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/companies?${queryString}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setCompanies(data.data);
+          let temp = cachedCategoryCompanies;
+          temp.push({category_id : category_id , companies : data.data})
+          addCachedCategoryCompanes(temp);
+        });
+    },[])
+
     useEffect(() => {
         fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/categories`)
           .then((res) => res.json())
