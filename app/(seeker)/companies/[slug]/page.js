bb8e1c22aa798ea46913677f171da6590bbbdf25
Ahmed Kibria <gkibria121@gmail.com>
2024-04-02 16:29:18 +0600

    get circular of a company

--- app/(seeker)/companies/[slug]/page.js
+++ app/(seeker)/companies/[slug]/page.js
@@ -43,7 +43,7 @@ function Page() {
         })
     },[])
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/circular`)
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/circular/${slug}`)
             .then(res => res.json())
             .then(data => {
                 let jobs = data.data;
