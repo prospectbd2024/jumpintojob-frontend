c7959ba9f58031c933c9206740162c380e48db84
gkibria <gkibria121@gmail.com>
2024-04-15 17:20:08 +0600

    bug fix, docker

--- bin/deploy_dev.sh
+++ bin/deploy_dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 FILES="-f docker-compose.yml"
 NAME="jumpintojob-frontend.pid"
-# if [[ ! -e .env ]]
-# then
-#     cp .env.example .env
-# fi
+if [[ ! -e .env.local ]]
+then
+    cp .env.local.docker.exmaple .env.local
+fi
 # Check if the PID file exists and delete it if so
 docker compose $FILES down
 if [ -f "$LOCALAPPDATA/docker-compose/$NAME" ]; then
