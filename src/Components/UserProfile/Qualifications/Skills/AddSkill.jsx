80547a4e10451e7884b7d24f4db4c339a20e2f0e
gkibria <gkibria121@gmail.com>
2024-05-08 13:40:11 +0600

    add skill

---
+++ src/Components/UserProfile/Qualifications/Skills/AddSkill.jsx
@@ -0,0 +1,29 @@
+// AddSkill.js
+import React,{useEffect}  from "react";
+import SkillInput from "./SkillInput";
+import Suggestions from "./Suggestions";
+import "./AddSkill.css";
+import SkillProperties from "./SkillProperties";
+
+function AddSkill({props}) {
+    const {selectedSkill,skillErrors,setSkill} =props;
+    
+  return (
+    <div className="add-skill-container">
+      <p className="add-skill-heading">
+        Add your top skills to impress the employers
+      </p>
+      <SkillInput props={{setSkill,selectedSkill}} />
+      <Suggestions props={{setSkill,selectedSkill}} />
+      <div className='skill-error'>{ Object.keys(skillErrors).map ((key)=>{
+        return <p key={key}>{skillErrors[key]}</p>
+      })}</div>
+      {
+selectedSkill.name &&
+      <SkillProperties    props={{setSkill,selectedSkill}}/>
+      }
+    </div>
+  );
+}
+
+export default AddSkill;
