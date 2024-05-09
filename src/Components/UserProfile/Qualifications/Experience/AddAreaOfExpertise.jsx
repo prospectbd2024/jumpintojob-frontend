7e7fba4619356259fc33729c6f39a88e8387866c
gkibria <gkibria121@gmail.com>
2024-05-09 15:29:01 +0600

    style change

--- src/Components/UserProfile/Qualifications/Experience/AddAreaOfExpertise.jsx
+++ src/Components/UserProfile/Qualifications/Experience/AddAreaOfExpertise.jsx
@@ -1,42 +1,43 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
 function AddAreaOfExpertise({ props }) {
-  const { experience, setExperience,experienceErrors } = props;
-  const [expertises, setExpertises] = useState([]);
+  const { experience, setExperience, experienceErrors } = props;
   const [maxError, setMaxError] = useState("");
 
   const handleChange = (index, field, value) => {
-    const newExpertises = [...expertises];
+    const newExpertises = [...experience.expertises];
     newExpertises[index] = { ...newExpertises[index], [field]: value };
-    setExpertises(newExpertises);
+    setExperience((prevExperience) => ({
+      ...prevExperience,
+      expertises: newExpertises,
+    }));
   };
 
   const handleDelete = (index) => {
-    const newExpertises = [...expertises];
+    const newExpertises = [...experience.expertises];
     newExpertises.splice(index, 1);
-    setExpertises(newExpertises);
+    setExperience((prevExperience) => ({
+      ...prevExperience,
+      expertises: newExpertises,
+    }));
   };
-  useEffect(()=>{
-    if(expertises.length>0){
-        setExperience(prev=>({...prev, expertises : expertises }))
-    }
-  },[expertises])
 
   const handleAdd = () => {
-    // Check if any expertise entry has empty name or months
-    const hasEmptyFields = expertises.some((element) => !element.name || !element.months);
-  
-    if (expertises.length < 3 && !hasEmptyFields) {
-      setExpertises([...expertises, { name: "", months: "" }]);
-    } else {
-      
-        if (hasEmptyFields){
-            setMaxError("Name or months is missing");
-        }
-        else{
-            setMaxError("You can only add a maximum of 3 areas of expertise");
-        }
+    if (experience?.expertises?.length < 3) {
+      setExperience((prevExperience) => ({
+        ...prevExperience,
+        expertises: [...prevExperience.expertises, { name: "", months: "" }],
+      }));
+    }
+    else if (!experience.expertises){
+      setExperience((prevExperience) => ({
+        ...prevExperience,
+        expertises: [ { name: "", months: "" }],
+      }));
+    }
+     else {
+      setMaxError("You can only add a maximum of 3 areas of expertise");
       setTimeout(() => {
         setMaxError("");
       }, 3000); // Remove error message after 3 seconds
@@ -55,7 +56,7 @@ function AddAreaOfExpertise({ props }) {
         </span>
       </div>
       <div>
-        {expertises.map((element, index) => (
+        {experience?.expertises?.map((element, index) => (
           <div className="add-experience-experties" key={index}>
             <div className="">
               <input
@@ -65,16 +66,15 @@ function AddAreaOfExpertise({ props }) {
               />
             </div>
             <div className="">
-            <input
-            type="number"
-            className=""
-            placeholder="Months"
-            style={{ width: "60px" , outline : 'none'}}
-            min="0"
-            value={element.months}
-            onChange={(e) => handleChange(index, "months", e.target.value)}
-            />
-
+              <input
+                type="number"
+                className=""
+                placeholder="Months"
+                style={{ width: "60px", outline: "none" }}
+                min="0"
+                value={element.months}
+                onChange={(e) => handleChange(index, "months", e.target.value)}
+              />
             </div>
             <div className="delete-btn-container">
               <FaTrashAlt
@@ -87,17 +87,14 @@ function AddAreaOfExpertise({ props }) {
       </div>
 
       <div id="addButton" className="" style={{ display: "block" }}>
-        <button
-          className="add-experience-experties-add-btn"
-          onClick={handleAdd}
-        >
+        <button className="add-experience-experties-add-btn" onClick={handleAdd}>
           <i className=" "></i>Add New
         </button>
       </div>
       <div className="max-error">{maxError}</div>
-     {experienceErrors.expertises && <div className='max-error'>
-        Please add Experties
-     </div> }
+      {experienceErrors.expertises && (
+        <div className="max-error">Please add Expertises</div>
+      )}
       <input type="hidden" name="userType" id="userType" value="" />
     </div>
   );
