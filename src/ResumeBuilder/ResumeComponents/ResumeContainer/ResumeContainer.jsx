fcc9a5349a693afe4a14fa0f1513515a46cae039
gkibria <gkibria121@gmail.com>
2024-05-13 23:08:32 +0600

    code restructure

--- src/ResumeBuilder/ResumeComponents/ResumeContainer/ResumeContainer.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeContainer/ResumeContainer.jsx
@@ -1,59 +1,55 @@
-"use client"
-import React, { useState } from 'react';
-import ResumeSteps from '../ResumeSteps/ResumeSteps';
-import ResumeTemplates from '../ResumeTemplates/ResumeTemplates';
-import { useUserContext } from '../../../Contexts/UserContext';
-import ResumeHeading from '../ResumeHeading/ResumeHeading';
-import ResumeEducation from '../ResumeEducation/ResumeEducation';
-import ResumePreview from '../ResumePreview/ResumePreview';
-import ResumeExperiences from '../ResumeExperiences/ResumeExperiences';
-import ResumeSkills from '../ResumeSkills/ResumeSkills';
-import ResumeFinalize from '../ResumeFinalize/ResumeFinalize';
-import SkillContextProvider from '@/Contexts/SkillContext';
+"use client";
+import React, { useState } from "react";
+import ResumeSteps from "../ResumeSteps/ResumeSteps";
+import ResumeTemplates from "../ResumeTemplates/ResumeTemplates";
+import ResumeHeading from "../ResumeHeading/ResumeHeading";
+import ResumeEducation from "../ResumeEducation/ResumeEducation";
+import ResumePreview from "../ResumePreview/ResumePreview";
+import ResumeExperiences from "../ResumeExperiences/ResumeExperiences";
+import ResumeSkills from "../ResumeSkills/ResumeSkills";
+import ResumeFinalize from "../ResumeFinalize/ResumeFinalize";
+import SkillContextProvider from "@/Contexts/SkillContext";
+import PrevNextButton from "@/ResumeBuilder/Layout/Button/PrevNextButton";
+import { useResumeContext } from "@/Contexts/ResumeContext";
 
 const ResumeContainer = () => {
-    const { currentStep, setCurrentStep, resumeData, setResumeData } = useUserContext();
-    const [templateType, setTemplateType] = useState(null);
-    const [selectedTemplateId, setSelectedTemplateId] = useState(null);
-    const [selectedTemplate, setSelectedTemplate] = useState([]);
+  const { currentStep, setCurrentStep } = useResumeContext();
+  const [templateType, setTemplateType] = useState(null);
+  const [selectedTemplateId, setSelectedTemplateId] = useState(null);
+  const [selectedTemplate, setSelectedTemplate] = useState([]);
 
+  const handleSelectedTemplate = (template) => {
+    setSelectedTemplateId(template.id);
+    setTemplateType(template.type);
+    setSelectedTemplate(template);
+  };
 
+  return (
+    <div style={{ padding: "50px 0" }} className="container">
+      <ResumeSteps />
+      <div
+        className="resume-content"
+        style={{ padding: "30px", margin: "50px 0" }}
+      >
+        {currentStep == 1 && (
+          <ResumeTemplates
+            handleSelectedTemplate={handleSelectedTemplate}
+            selectedTemplateId={selectedTemplateId}
+          />
+        )}
+        <div className="resume-form"  hidden={currentStep==1}>
+          {currentStep == 2 && <ResumeHeading />}
+          {currentStep == 3 && <ResumeEducation />}
+          {currentStep == 4 && <ResumeExperiences />}
+          {currentStep == 5 &&  <ResumeSkills /> }
+          {currentStep==6  && <ResumeFinalize />}
 
-
-    const handleSelectedTemplate = (template) => {
-        setSelectedTemplateId(template.id);
-        setTemplateType(template.type)
-        setSelectedTemplate(template);
-
-    }
-    // console.log(resumeData)
-    const handleCurrentStep = (step) => {
-
-        if(step==1){
-            return  <ResumeTemplates handleSelectedTemplate={handleSelectedTemplate} selectedTemplateId={selectedTemplateId} />
-        }
-
-        return <div style={{  }}>
-                    <div className="resume-form">
-                       {step==2 && <ResumeHeading templateType={templateType} />}
-                       {step==3 && <ResumeEducation />}
-                       {step==4 &&  <ResumeExperiences /> }
-                       {step==5 &&   <SkillContextProvider><ResumeSkills /></SkillContextProvider>  }
-                       {step==6 &&  <ResumeFinalize /> }
-                    </div>
-                    {/* <div className="resume-preview">
-                        <ResumePreview selectedTemplate={selectedTemplate} />
-                    </div> */}
-                </div>
-    }
-    return (
-        <div style={{ padding: '50px 0' }} className='container'>
-            <ResumeSteps />
-            <div className="resume-content" style={{ padding: '30px', margin: '50px 0' }}>
-                {handleCurrentStep(currentStep)}
-            </div>
+          <PrevNextButton props={{setCurrentStep,currentStep}} />
+     
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ResumeContainer;
