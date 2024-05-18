338644ff3643980ad88f45e96a059e511b9c463e
Ahmed Kibria <gkibria121@gmail.com>
2024-05-18 19:21:08 +0600

    id added

--- src/ResumeBuilder/ResumeComponents/ResumeFinalize/ResumeOthers.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeFinalize/ResumeOthers.jsx
@@ -19,23 +19,23 @@ function ResumeOthers() {
     <div>
     <ul className="addmore">
         <li>
-            <input type="radio" value="projects" name="add" checked={inputType=='projects'}   onChange={handleChange}/>
+            <input type="radio" id="projects" value="projects" name="add" checked={inputType=='projects'}   onChange={handleChange}/>
             <label htmlFor="projects">Add projects</label>
         </li>
         <li>
-            <input type="radio" value="certificates" name="add" onChange={handleChange}/>
+            <input type="radio" id="certificates" value="certificates" name="add" onChange={handleChange}/>
             <label htmlFor="certificates">Add certificates</label>
         </li>
         <li>
-            <input type="radio" value="publications" name="add" onChange={handleChange} />
+            <input type="radio" value="publications"   id="publications"name="add" onChange={handleChange} />
             <label htmlFor="publications">Add publications</label>
         </li>
         <li>
-            <input type="radio" value="hobbies" name="add" onChange={handleChange}/>
+            <input type="radio" value="hobbies"  id="hobbies" name="add" onChange={handleChange}/>
             <label htmlFor="hobbies">Add hobbies</label>
         </li>
         <li>
-            <input type="radio" value="more" name="add" onChange={handleChange}/>
+            <input type="radio" value="more"  id="more" name="add" onChange={handleChange}/>
             <label htmlFor="more">Other</label>
             {/* <input type="text" value="more" /> */}
         </li>
