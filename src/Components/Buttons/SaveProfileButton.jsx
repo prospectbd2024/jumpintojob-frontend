2f1abd50a07bf45358eb7f488f39c8845777f0dc
Ahmed Kibria <gkibria121@gmail.com>
2024-05-26 21:32:05 +0600

    saveprofile

--- src/Components/Buttons/SaveProfileButton.jsx
+++ src/Components/Buttons/SaveProfileButton.jsx
@@ -2,9 +2,20 @@ import { useUserProfileContext } from '@/Contexts/UserProfileContext'
 import React from 'react'
 
 function SaveProfileButton() {
-    const {saveProfile} = useUserProfileContext();
+  const { saveProfile } = useUserProfileContext();
   return (
-    <button onClick={saveProfile} style={{ background: "var(--primary-color)", color: "#fff", width: "120px", height: "40px", borderRadius: "5px", border: "none", fontSize: "16px", fontWeight: "bold", cursor: "pointer", transition: ".3s" }}>save</button>
+    <button onClick={saveProfile} 
+    style={{ 
+      background: "var(--primary-color)", 
+      color: "#fff", 
+      width: "120px", 
+      height: "40px",
+      borderRadius: "5px", 
+      border: "none", 
+      fontSize: "16px", 
+      fontWeight: "bold", 
+      cursor: "pointer", 
+      transition: ".3s" }}>save</button>
   )
 }
 
