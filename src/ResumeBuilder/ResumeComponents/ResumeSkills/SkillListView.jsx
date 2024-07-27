3e5481a5d772e87623b1980005c341b53ef9de3c
A B S Talha <abstalha192@gmail.com>
2024-07-27 12:27:35 +0600

    made some major changes in the skill section

--- src/ResumeBuilder/ResumeComponents/ResumeSkills/SkillListView.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeSkills/SkillListView.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
-function SkillListView({skills,onSkillClick}) {
-
+function SkillListView({skills, onSkillClick}) {
   return (
     <ul>
-    {skills &&
-      skills?.map((skill,index) => {
-        return (
-          <div key={index}>
-            <li onClick={() =>{ onSkillClick(skill)}}   style={{'cursor' : 'pointer' ,'listStyle' : 'none'}} >{skill.name}  </li>
-          </div>
-        );
-      })}
-  </ul>
+      {skills &&
+        skills?.map((skill, index) => {
+          return (
+            <div key={index}>
+              <li 
+                onClick={() => { onSkillClick(skill) }}   
+                style={{'cursor': 'pointer', 'listStyle': 'none'}} 
+              >
+                {skill.name}
+              </li>
+            </div>
+          );
+        })}
+    </ul>
   )
 }
 
