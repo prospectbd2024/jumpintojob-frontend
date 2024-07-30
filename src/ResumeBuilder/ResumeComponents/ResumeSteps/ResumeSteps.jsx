28544d0bd168c465607b3a02eb7959715e43729e
gkibria121 <gkibria121@gmail.com>
2024-07-30 16:21:43 +0600

    BEM naming convention added to resumeSteps

--- src/ResumeBuilder/ResumeComponents/ResumeSteps/ResumeSteps.jsx
+++ src/ResumeBuilder/ResumeComponents/ResumeSteps/ResumeSteps.jsx
@@ -11,51 +11,51 @@ const ResumeSteps = () => {
     return (
         <div className='resume-progress u-margin-top-medium'>
             <div className="resume-progress__container">
-                <div className="steps">
-                    <div className={`step ${currentStep >= 1 ? 'active-step' : ''}`} onClick={()=>{changeStep(1)}}>
-                        <div className='step-title-container' >
+                <div className="resume-progress__steps">
+                    <div className={`resume-progress__step ${currentStep >= 1 ? 'active-step' : ''}`} onClick={()=>{changeStep(1)}}>
+                        <div className='resume-progress__title-container' >
                             <h5>1</h5>
                             <p>TEMPLATE</p>
                         </div>
                         <span></span>
                     </div>
-                    <div className={`step ${currentStep >= 2 ? 'active-step' : ''}`} onClick={()=>{changeStep(2)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 2 ? 'active-step' : ''}`} onClick={()=>{changeStep(2)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>2</h5>
                             <p>HEADER</p>
                         </div>
                         <span ></span>
                     </div>
-                    <div className={`step ${currentStep >= 3 ? 'active-step' : ''}`} onClick={()=>{changeStep(3)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 3 ? 'active-step' : ''}`} onClick={()=>{changeStep(3)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>3</h5>
                             <p>EDUCATION</p>
                         </div>
                         <span ></span>
                     </div>
-                    <div className={`step ${currentStep >= 4 ? 'active-step' : ''}`} onClick={()=>{changeStep(4)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 4 ? 'active-step' : ''}`} onClick={()=>{changeStep(4)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>4</h5>
                             <p>EXPERIENCE</p>
                         </div>
                         <span></span>
                     </div>
-                    <div className={`step ${currentStep >= 5 ? 'active-step' : ''}`} onClick={()=>{changeStep(5)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 5 ? 'active-step' : ''}`} onClick={()=>{changeStep(5)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>5</h5>
                             <p>SKILLS</p>
                         </div>
                         <span ></span>
                     </div>
-                    <div className={`step ${currentStep >= 6 ? 'active-step' : ''}`} onClick={()=>{changeStep(6)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 6 ? 'active-step' : ''}`} onClick={()=>{changeStep(6)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>6</h5>
                             <p>FINALIZE</p>
                         </div>
                         <span></span>
                     </div>
-                    <div className={`step ${currentStep >= 7 ? 'active-step' : ''}`} onClick={()=>{changeStep(7)}}>
-                        <div className='step-title-container'>
+                    <div className={`resume-progress__step ${currentStep >= 7 ? 'active-step' : ''}`} onClick={()=>{changeStep(7)}}>
+                        <div className='resume-progress__title-container'>
                             <h5>7</h5>
                             <p>Preview</p>
                         </div>
