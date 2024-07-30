8daf0f326914e51e534e3813ab5fcfa221faae96
gkibria121 <gkibria121@gmail.com>
2024-07-30 20:12:31 +0600

    add sass

--- src/ResumeBuilder/Layout/ResumeFooter/ResumeFooter.jsx
+++ src/ResumeBuilder/Layout/ResumeFooter/ResumeFooter.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import  Link  from 'next/link';
+import Link from 'next/link';
 import './ResumeFooter.scss'
 
 const ResumeFooter = () => {
     return (
         <footer className='resume-footer'>
-            <div className="resume-footer-container container">
-                <div className="resume-footer-content">
-                    <div>
-                        <ul>
-                            <p>2023 &copy; jobshub</p>
-                            <li><Link href="">Do not sell my data</Link></li>
-                            <li><Link href="">Terms and conditions</Link></li>
-                        </ul>
-                        <ul>
-                            <li><Link href="/findjobs">Browse jobs</Link></li>
-                            <li><Link href="/companies">Browse companies</Link></li>
-                        </ul>
-
-                    </div>
+            <div className="resume-footer__container container">
+                <div className="resume-footer__content">
+                    <ul className="resume-footer__content-list">
+                        <li className="resume-footer__content-list-item">
+                            <p className="resume-footer__content-list-text">2023 &copy; jobshub</p>
+                        </li>
+                        <li className="resume-footer__content-list-item">
+                            <Link href="" className="resume-footer__content-list-link">Do not sell my data</Link>
+                        </li>
+                        <li className="resume-footer__content-list-item">
+                            <Link href="" className="resume-footer__content-list-link">Terms and conditions</Link>
+                        </li>
+                    </ul>
+                    <ul className="resume-footer__content-list">
+                        <li className="resume-footer__content-list-item">
+                            <Link href="/findjobs" className="resume-footer__content-list-link">Browse jobs</Link>
+                        </li>
+                        <li className="resume-footer__content-list-item">
+                            <Link href="/companies" className="resume-footer__content-list-link">Browse companies</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
