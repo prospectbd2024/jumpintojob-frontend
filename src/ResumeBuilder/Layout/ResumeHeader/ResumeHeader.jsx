35f8ea193245f7095910854d33d8ded6303448ac
gkibria121 <gkibria121@gmail.com>
2024-08-03 12:02:36 +0600

    add tailwind to resume header

--- src/ResumeBuilder/Layout/ResumeHeader/ResumeHeader.jsx
+++ src/ResumeBuilder/Layout/ResumeHeader/ResumeHeader.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
-import './ResumeHeader.css';
 import Logo from '@/assets/Logo';
 
 const ResumeHeader = () => {
     return (
-        <header className='resume-header'>
-            <div className="resume-header-container container">
-                <div className="resume-header-content">
-                    <div className="resume-header-title">
-                        <Logo className='jump-job-logo' fill='var(--primary-color)' />
-                        <span className='title'>Resume Builder</span>
+        <header className="border-b border-gray-300 py-4">
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between gap-2">
+                    <div className="flex items-center gap-2">
+                        <Logo className="w-36 h-12" fill="var(--primary-color)" />
+                        <span className="text-lg font-bold text-[var(--primary-color)] ml-4">Resume Builder</span>
                     </div>
-                    <div className="resume-header-button">
-                        <Link href="/findjobs"><button>Job Board</button></Link>
+                    <div>
+                        <Link href="/findjobs">
+                            <button className="w-32 h-11 font-bold rounded bg-[var(--primary-color)] text-white border-none text-base cursor-pointer transition duration-200 hover:bg-[#2f89c5]">
+                                Job Board
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
