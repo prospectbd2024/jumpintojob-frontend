6562eb5691a8b33c65cdffe669d7f95953fe01d4
gkibria121 <gkibria121@gmail.com>
2024-08-03 12:06:43 +0600

    add tailwind to resume footer

--- src/ResumeBuilder/Layout/ResumeFooter/ResumeFooter.jsx
+++ src/ResumeBuilder/Layout/ResumeFooter/ResumeFooter.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import  Link  from 'next/link';
-import './ResumeFooter.css'
+import Link from 'next/link';
 
 const ResumeFooter = () => {
     return (
-        <footer className='resume-footer'>
-            <div className="resume-footer-container container">
-                <div className="resume-footer-content">
-                    <div>
-                        <ul>
-                            <p>2023 &copy; jobshub</p>
-                            <li><Link href="">Do not sell my data</Link></li>
-                            <li><Link href="">Terms and conditions</Link></li>
-                        </ul>
-                        <ul>
-                            <li><Link href="/findjobs">Browse jobs</Link></li>
-                            <li><Link href="/companies">Browse companies</Link></li>
-                        </ul>
-
-                    </div>
+        <footer className="w-full py-5 border-t border-gray-300">
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between">
+                    <ul className="flex gap-5">
+                        <p className="text-gray-600 text-sm">2023 &copy; jobshub</p>
+                        <li className="list-none">
+                            <Link href="" className="text-blue-500 text-sm transition duration-100 hover:text-black">
+                                Do not sell my data
+                            </Link>
+                        </li>
+                        <li className="list-none">
+                            <Link href="" className="text-blue-500 text-sm transition duration-100 hover:text-black">
+                                Terms and conditions
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className="flex gap-5">
+                        <li className="list-none">
+                            <Link href="/findjobs" className="text-blue-500 text-sm transition duration-100 hover:text-black">
+                                Browse jobs
+                            </Link>
+                        </li>
+                        <li className="list-none">
+                            <Link href="/companies" className="text-blue-500 text-sm transition duration-100 hover:text-black">
+                                Browse companies
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
