7c3daa03f3d198208a121a7d8844810c2482fc8a
gkibria121 <gkibria121@gmail.com>
2024-08-03 12:05:38 +0600

    add tailwind to resume buttons

--- src/ResumeBuilder/Layout/Button/SaveDismissButtons.jsx
+++ src/ResumeBuilder/Layout/Button/SaveDismissButtons.jsx
@@ -1,61 +1,37 @@
-import React, { useCallback } from 'react'
+import React, { useCallback } from 'react';
 
-function SaveDismissButtons({props}) {
-
-
-  const isRequiredMissing = (array)=>{
-    const values = array 
-    let rv = false
-    values.forEach((field) =>{
-      if (field==''){
-        rv= true
+function SaveDismissButtons({ props }) {
+  const isRequiredMissing = (array) => {
+    let rv = false;
+    array.forEach((field) => {
+      if (field === '') {
+        rv = true;
       }
-    }) 
+    });
+    return rv;
+  };
 
-    return rv
-  }
-
-
-  const {state,setState,id,setWarning,requiredFields} = props;
+  const { state, setState, setWarning, requiredFields } = props;
+  
   const saveChanges = useCallback(() => {
-    // console.log(state)
-
-    if(isRequiredMissing(requiredFields)){
-      setWarning(true)
+    if (isRequiredMissing(requiredFields)) {
+      setWarning(true);
+    } else {
+      setState({ ...state, type: "list-view" });
     }
-    else{
-      setState({...state, type: "list-view"});
-    
-    }
-
-  });
+  }, [state, setState, setWarning, requiredFields]);
 
   return (
-
     <button
       onClick={(e) => {
         e.preventDefault();
-
-
         saveChanges();
       }}
-      style={{
-        height: '35px',
-        width: '76px',
-        backgroundColor: 'rgb(52, 152, 219)',
-        textAlign: 'center',
-        paddingTop: '0px',
-        color: 'white',
-        borderRadius: '5px',
-        cursor: 'pointer',
-        border: '1px solid rgb(52, 152, 219)',
-        marginTop: '18px'
-      }}
+      className="h-9 w-20 bg-blue-500 text-white text-center rounded cursor-pointer border border-blue-500 mt-4"
     >
       Save
     </button>
-
-  )
+  );
 }
 
-export default SaveDismissButtons
+export default SaveDismissButtons;
