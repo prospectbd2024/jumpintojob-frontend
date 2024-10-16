ccb43605e98a2968a372adb7f72b7aaac6e78882
mehedi <mehidy.gb@gmail.com>
2024-10-16 12:15:15 +0600

    set dashboard context into layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,40 +1,43 @@
 import React from "react";
 import "./globals.css";
 import "@/EmployersComponents/ForEmployersHome/ForEmployersLiveData/ForEmployersLiveData.css";
-import "@/EmployersComponents/ForEmployersPostJob/ForEmployersPostJob.css"; 
-import { UserProvider } from "@/Contexts/UserContext";
+import "@/EmployersComponents/ForEmployersPostJob/ForEmployersPostJob.css";
+import {UserProvider} from "@/Contexts/UserContext";
 import "./variables.css";
 import "./style.css";
 import JobContext from "@/Contexts/JobContext";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "app/api/auth/[...nextauth]/route";
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "app/api/auth/[...nextauth]/route";
 import Provider from "@/UserContext/SessionProvider";
 import CompanyContext from "@/Contexts/CompanyContext";
 import CategoryContext from "@/Contexts/CategoryContext";
 import UserProfileContext from "@/Contexts/UserProfileContext";
 import ApplicationContext from "@/Contexts/ApplicationContext";
+import DashboardContext from "@/Contexts/DashboardContext";
 
-async function layout({ children }) {
-  const session = await getServerSession(authOptions);
-  return (
-    <html lang="en">
-      <body className="fou">
+async function layout({children}) {
+    const session = await getServerSession(authOptions);
+    return (
+        <html lang="en">
+        <body className="fou">
         <UserProvider>
-          <JobContext>
-            <CompanyContext>
-              <CategoryContext>
-                <UserProfileContext>
-                  <ApplicationContext>
-                    <Provider session={session}>{children}</Provider>
-                  </ApplicationContext>
-                </UserProfileContext>
-              </CategoryContext>
-            </CompanyContext>
-          </JobContext>
+            <JobContext>
+                <CompanyContext>
+                    <CategoryContext>
+                        <UserProfileContext>
+                            <DashboardContext>
+                                <ApplicationContext>
+                                    <Provider session={session}>{children}</Provider>
+                                </ApplicationContext>
+                            </DashboardContext>
+                        </UserProfileContext>
+                    </CategoryContext>
+                </CompanyContext>
+            </JobContext>
         </UserProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
 
 export default layout;
